09ca8c4349e601c05ceaf21963ad74050cf29862
Igor Dvorkin <idvorkin@gmail.com>
2021-07-03 15:40:53 -0700

    Save keepwarm.sh

--- keepwarm.sh
+++ keepwarm.sh
@@ -4,11 +4,16 @@ while true; do
         # woah - |& redirect stdout to tee - who knew!
         (time http https://manager-book.azurewebsites.net/topic\?t\=what-are-your-thoughts-on-a-pip) 2>&1 | tee -a ~/tmp/keepwarm.out.txt
         date | tee -a ~/tmp/keepwarm.out.txt
-        sleep 600 # sleep for 5 minutes (which is I think what required to stay warm)
+        sleep 60 # sleep for 5 minutes (which is I think what required to stay warm)
 
-            # per month
-            # 12/hr * 24 hr/day *30 day/month
-            # ~ 5K /month
+        # with a 1 minute sleep:
+        # 60/hr * 24 hr/day *30 day/month
+        # ~ 50K /month
+        # App is 1.5GB, ~ excution is ~1s.
+        # ~ 100K GB.s
+        # https://azure.microsoft.com/en-us/pricing/details/functions/
+        # Free: 1M requests, 400K GB*s
+        # Still lots of headroom
 
 done
 
