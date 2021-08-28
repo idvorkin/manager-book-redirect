249e3dd218a13a0b305763f88e38f88702d922e7
Igor Dvorkin <idvorkin@gmail.com>
2021-08-28 09:35:25 -0700

    Add tests and deployment info

--- run-tests.sh
+++ run-tests.sh
@@ -2,3 +2,16 @@
 # setup code:
 # python3 -m venv .venv
 . .venv/bin/activate && pytest
+
+echo "Deployed on Staging"
+http https://manager-book-dev.azurewebsites.net/
+http https://manager-book-dev.azurewebsites.net/manager-book-anchor
+http https://manager-book-dev.azurewebsites.net/random-page/a-anchor
+
+echo "Deployed on Prod"
+
+http https://manager-book.azurewebsites.net/
+http https://manager-book.azurewebsites.net/manager-book-anchor
+http https://manager-book.azurewebsites.net/random-page/a-anchor
+
+Pushing to deploy-prod deploys to  https://manager-book.azurewebsites.net
